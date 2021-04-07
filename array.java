094b0886cf2c22a7ce815c9cd9b932bc6f7a1944
chandu256 <81565889+chandu256@users.noreply.github.com>
2021-04-07 09:16:55 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,12 @@
+class array
+{
+public static void main(String a[])
+{
+int array []=new int[]{4,5,9,8,6};
+System.out.println("Elements of given array");
+for(int i=0;i<array.length;i++)
+{
+System.out.println(array[i]+" ");
+}
+}
+}
